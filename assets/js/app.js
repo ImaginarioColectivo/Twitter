1026115fa90b9bd7b145f310aaffef359f0cab22
ImaginarioColectivo <marjourzua@gmail.com>
2017-11-12 19:52:21 -0300

    actualizando twitter

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,48 @@
+function add(){
+	//tomamos el texto del textarea
+	var coments = document.getElementById("comment").value;
+	//limpiamos el textarea
+	document.getElementById("comment").value = "";
+	//llamamos al contendor vacio
+	var contenedor = document.getElementById("contenedor");
+	//crear el div que contiene el comentario
+	var newComments = document.createElement("div");
+	//desabilitamos el boton a campo vacio
+	if (coments.length ==0 || coments == null){
+		return document.getElementById("btn").disabled = true;
+	}
+	else {
+
+	};
+	//creamos nodo de texto
+
+	var textNewComments = document.createTextNode(coments);
+	var contendedorComment = document.createElement("p");
+
+	//agrego el coment a p 
+	contendedorComment.appendChild(textNewComments);
+	// agregar p al div vacio
+	newComments.appendChild(contendedorComment);
+
+	//agregar la hora al nuevo comentario
+	var horaActual = new Date();
+	var hora = horaActual.getHours();
+	var minutos = horaActual.getUTCMinutes();
+
+	//creamos nodo de texto que contendra la hora actual
+	var horaReal = document.createTextNode(hora + ":" + minutos);
+
+	//la hora la guardamos en un elemento nuevo
+	var horaFinal = document.createElement("p");
+
+	//agregamos p y hora en el contenido final
+	horaFinal.appendChild(horaReal); 
+	newComments.appendChild(horaFinal);
+	//agregar  al contendedor vacio el div 
+	contenedor.appendChild(newComments);
+}
+
+//para que el textarea crezca
 var textarea = document.getElementById("textarea");
 var limit = 200;
 
