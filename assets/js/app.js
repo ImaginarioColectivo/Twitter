27986339958221f84ab74733e7f5b8bbcb448b88
ImaginarioColectivo <marjourzua@gmail.com>
2017-11-12 23:50:08 -0300

    final

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,12 @@ function add(){
 	//tomamos el texto del textarea
 	var coments = document.getElementById("comment").value;
 	//limpiamos el textarea
-	document.getElementById("comment").value = "";
+	document.getElementById("comment").value = '';
 	//llamamos al contendor vacio
 	var contenedor = document.getElementById("contenedor");
 	//crear el div que contiene el comentario
 	var newComments = document.createElement("div");
+	
 	//desabilitamos el boton a campo vacio
 	if (coments.length ==0 || coments == null){
 		return document.getElementById("btn").disabled = true;
@@ -39,14 +40,43 @@ function add(){
 	horaFinal.appendChild(horaReal); 
 	newComments.appendChild(horaFinal);
 	//agregar  al contendedor vacio el div 
-	contenedor.appendChild(newComments);
+	contenedor.appendChild(newComments); 
+	
+}
+//para agrandar el textarea pero no me funciona
+
+function textAreaAdjust(o){
+	o.style.height = "1px";
+	o.style.height = ( 25+o.scrollHeight) + "px";
 }
 
-//para que el textarea crezca
-var textarea = document.getElementById("textarea");
-var limit = 200;
+//este es el contador
+var textarea = document.getElementById("comment");
+comment.onkeydown = function(){
+	var comment = document.getElementById("comment");
+	var guardarComment = comment.value.length;
+	var contador = document.getElementById("caracter");
+	contador.innerHTML = 140- guardarComment;
 
-textarea.oninput = function() {
-  textarea.style.height = "";
-  textarea.style.height = Math.min(textarea.scrollHeight, 300) + "px";
-};
+	if (guardarComment >= 120 && guardarComment < 130){
+		return contador.setAttribute("class","blue");
+	}
+
+	if (guardarComment >= 130 && guardarComment <140){
+		return contador.setAttribute("class","violet");
+
+	} else {
+
+	};
+
+	if (guardarComment > 140) {
+		return document.getElementById("btn").disabled = true;
+
+	} else {
+		return document.getElementById("btn").enabled = true;
+
+	}
+
+	}
+
+
